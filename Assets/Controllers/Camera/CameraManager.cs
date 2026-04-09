39dc217fe2a69777bccb4957a84c1a2e95c40674
Jakovage <jakovage@gmail.com>
2026-04-09 04:45:17 -0400

    add camera scrolling
    forgot to make a branch for this so whatever, committing on main. but like the commit message says, i made the camera gradually accelerate up.
    
    by the way, the platforms are not randomly generated. i placed them all manually just to test it out. that will be the next feature we work on! (the random generation that is!)
    
    also removed the .vsconfig from the remote repository and added it to the .gitignore

---
+++ Assets/Controllers/Camera/CameraManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private float _orthographicSize;
+    private Camera _cam;
+
+    [SerializeField] private float _gracePeriod; // seconds given before camera starts moving
+    [SerializeField] private float _initialAcceleration; // initial acceleration to get the camera up to speed
+    [SerializeField] private float _actualAcceleration; // the actual rate at which the camera will accelerate for the rest of the gameplay loop
+    [SerializeField] private float _endInitialAccelerationSpeed;
+    [SerializeField] private float _maxSpeed; // i mean yea
+
+    private float _timer;
+    private float _currentSpeed;
+
+    private void Awake()
+    {
+        _cam = GetComponent<Camera>();
+        _cam.orthographic = true;
+        _cam.orthographicSize = _orthographicSize;
+
+        _orthographicSize = 5f;
+        _timer = 0;
+        _currentSpeed = 0;
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        
+        if (_currentSpeed > _maxSpeed)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + _maxSpeed * Time.deltaTime, transform.position.z);
+        }
+        else if (_currentSpeed > _endInitialAccelerationSpeed) // acceleration should slow down once it has initially caught up to speed
+
+        {
+            _currentSpeed += _actualAcceleration * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, transform.position.y + _currentSpeed * Time.deltaTime, transform.position.z);
+        }
+        else if (_timer > _gracePeriod) // after grace period, camera should accelerate quickly to catch up
+        {
+            _currentSpeed += _initialAcceleration * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, transform.position.y + _currentSpeed * Time.deltaTime, transform.position.z);
+        }
+    }
+}
