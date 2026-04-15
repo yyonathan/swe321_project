94f99d372c74872a690947132f067f7a4b2cc24d
hoanggddo <hoangg.ddo@gmail.com>
2026-04-15 00:20:54 -0400

    Initial commit

---
+++ Assets/UI/Settings/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+
+    public void Settings()
+    {
+        SceneManager.LoadSceneAsync("Settings");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
